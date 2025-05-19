515f6a93710915c1ee25711aba3cefc40c08c92f
Leon.C <kindlyhearted666@gmail.com>
2025-05-19 11:40:15 +0900

    config.phpを除外

--- inc/db.php
+++ inc/db.php
@@ -1,9 +1,13 @@
 <?php
-    $host = 'localhost';
-    $port = '3306';
-    $dbname = 'xs981006_5exsi';
-    $user = 'xs981006_3ukoo';
-    $pass = 'mgz9zixzgx';
+    // config.php を読み込む
+    $config = require_once __DIR__ . '/../config.php';
+
+    $host = $config['host'];
+    $port = $config['port'];
+    $dbname = $config['dbname'];
+    $user = $config['user'];
+    $pass = $config['pass'];
+
     $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
     // エラーが出たときに、「例外（エラーの情報）」を投げるようにする設定
     $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
